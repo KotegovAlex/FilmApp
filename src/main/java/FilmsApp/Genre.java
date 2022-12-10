ab2f25d11b1fa2c50b685e0136908c845996c0f2
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-12-10 15:01:58 +0700

    Create Genre

--- src/main/java/FilmsApp/Genre.java
+++ src/main/java/FilmsApp/Genre.java
@@ -1,4 +1,19 @@
 package FilmsApp;
 
 public class Genre {
+    private Integer genreID;
+    private String genreName;
+
+    public Genre (Integer genreID, String genreName){
+        this.genreID = genreID;
+        this.genreName = genreName;
+    }
+
+    public Integer getGenreID() {
+        return genreID;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
 }
